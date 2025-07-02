3a20d84ea8c0ea6495e2f5451fa621231b63eaf0
ramzsanchez <ramilregensanchez@gmail.com>
2025-07-02 16:24:35 +0800

    updated links

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -27,7 +27,10 @@ export const Projects = () => {
               </div>
 
               <div className='flex justify-between items-center'>
-                <a href='#' className='text-blue-400 hover:text-blue-300 transition-colors my-4'>
+                <a
+                  href='https://ramzsanchez.github.io/vue-pomodoro/'
+                  className='text-blue-400 hover:text-blue-300 transition-colors my-4'
+                >
                   View Project →
                 </a>
               </div>
